3a9af28dee25447b52baadf79914f258ff6bb3c9
Shivansh Tiwari <shiv1shtiwari006@gmail.com>
2025-12-26 16:39:52 +0530

    changed some comments

--- Linked List/insertAtfront.cpp
+++ Linked List/insertAtfront.cpp
@@ -29,7 +29,7 @@ class List {
     void push_front(int val){
         Node * newNode = new Node(val);//dynamic cretion
         if (head == NULL){
-            head = tail = NULL;
+            head = tail = newNode;
         }
         else{
             newNode->next = head;
