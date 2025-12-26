0426cef4ce2048ba9576409b40660fb0cd48d39e
Shivansh Tiwari <shiv1shtiwari006@gmail.com>
2025-12-26 15:40:22 +0530

    Added the cretion.cpp fiel of linked list in the repo

---
+++ Linked List/creation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+           int data;
+           Node * next;
+
+           Node(int val){
+            data = val;
+            next = NULL;
+           }
+};
+
+class List{
+    public:
+    Node * head;
+    Node * tail;
+
+    List(){
+        head = NULL;
+        tail = NULL;
+    }
+};
+
+int main (){
+    List ll;
+
+    // this is just creation of node class which has data and next ptr.
+    // then created a List class that will be used to join all the elements(nodes) of the Node class to make it a linked list
+    // then creation of the linked list named ll
+}
